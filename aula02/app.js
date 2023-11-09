e9e240ce752ea4b50dcba20a0f82b29ae2508cae
Haroldo H. M. Mayworm <111655377+Haroldo-Mayworm@users.noreply.github.com>
2023-11-09 18:38:38 -0300

    Update - class_02

---
+++ aula02/app.js
@@ -0,0 +1,23 @@
+// const nome:string = ""
+// nome = prompt("Digite seu nome: ")
+// console.log(`Seja bem vindo, ${nome}!`)
+// _________________________________________________________
+// let num1:number, num2:number, n1:number, n2:number
+// num1 = prompt("Digite um número: ")
+// num2 = prompt("Digite outro número: ")
+// n1 = Number(num1)
+// n2 = Number(num2)
+// let soma:number, subt:number, divi:number, mult:number
+// soma = n1 + n2
+// subt = n1 - n2
+// divi = n1 / n2
+// mult = n1 * n2
+// console.log(`A soma de ${n1} + ${n2} é ${soma}`)
+// console.log(`A subtração de ${n1} - ${n2} é ${subt}`)
+// console.log(`A divisão de ${n1} / ${n2} é ${divi}`)
+// console.log(`A multiplicação de ${n1} * ${n2} é ${mult}`)
+// ___________________________________________________________
+var anoNascimento, idadeFutura;
+anoNascimento = prompt("Digite seu ano de nascimento: ");
+idadeFutura = 2030 - Number(anoNascimento);
+console.log(idadeFutura);
