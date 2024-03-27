c08776191f35b57280526ebfb97bb276cce6a915
Haroldo H. M. Mayworm <haroldomayworm@gmail.com>
2024-03-26 21:50:44 -0300

    Feat: customize my application

--- ReactNative/aula01/App.js
+++ ReactNative/aula01/App.js
@@ -1,11 +1,32 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Image, TouchableOpacity } from 'react-native';
 import styles from './assets/css/style';
 
 export default function App() {
+    const [backgroundColor, setBackgroundColor] = useState('#fff');
+    const [textColor, setTextColor] = useState('#000');
+
+    const color = () => {
+        if (backgroundColor === '#fff' && textColor === '#000') {
+            setBackgroundColor('#02066F');
+            setTextColor('#fff');
+        } else {
+            setBackgroundColor('#fff');
+            setTextColor('#000');
+        }
+    };
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>This is my app!!!</Text>
+        <View style={[styles.container, { backgroundColor }]}>
+            <Image source={require('./assets/imgs/LogoReactNative.png')} style={styles.img} />
+
+            <Text style={[styles.text, { color: textColor }]}>This is my app!</Text>
+
+            <TouchableOpacity onPress={color}>
+                <Text style={styles.btn}>change color</Text>
+            </TouchableOpacity>
+
             <StatusBar style="auto" />
         </View>
     );
--- ReactNative/aula01/assets/css/style.js
+++ ReactNative/aula01/assets/css/style.js
@@ -7,7 +7,20 @@ export default StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
+    img: {
+        width: 100,
+        height: 100,
+        marginBottom: 15,
+    },
     text: {
         fontSize: 30,
+        marginBottom: 50,
+    },
+    btn: {
+        backgroundColor: '#60D9FA',
+        textTransform: 'uppercase',
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+        borderRadius: 10,
     },
 });
